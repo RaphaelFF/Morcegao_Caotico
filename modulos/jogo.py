fd9532b63c74316e54818111fad4abd63ff8a4be
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2026-02-24 14:21:52 -0300

    feat: implementa aumento de velocidade e corrige erro de índice no spawn

--- modulos/jogo.py
+++ modulos/jogo.py
@@ -19,12 +19,12 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
     novo_cano2 = obter_cano_aleatorio(IMAGENS['cano'])
 
     canos_superiores = [
-        {'x': LARGURA_DA_TELA + 200, 'y': novo_cano1[0]['y']},
-        {'x': LARGURA_DA_TELA + 200 + (LARGURA_DA_TELA / 2), 'y': novo_cano2[0]['y']},
+        {'x': LARGURA_DA_TELA + 200, 'y': novo_cano1[0]['y'], 'passou': False},
+        {'x': LARGURA_DA_TELA + 200 + (LARGURA_DA_TELA / 2), 'y': novo_cano2[0]['y'], 'passou': False}
     ]
     canos_inferiores = [
-        {'x': LARGURA_DA_TELA + 200, 'y': novo_cano1[1]['y']},
-        {'x': LARGURA_DA_TELA + 200 + (LARGURA_DA_TELA / 2), 'y': novo_cano2[1]['y']},
+        {'x': LARGURA_DA_TELA + 200, 'y': novo_cano1[1]['y'], 'passou': False},
+        {'x': LARGURA_DA_TELA + 200 + (LARGURA_DA_TELA / 2), 'y': novo_cano2[1]['y'], 'passou': False},
     ]
 
     # Variáveis de física
@@ -62,18 +62,16 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
             }
 
         # Lógica da Pontuação
-        centro_passaro = posicao_passaro_x + IMAGENS['jogador'][0].get_width() / 2
-        for cano in canos_superiores:
-            centro_cano = cano['x'] + IMAGENS['cano'][0].get_width() / 2
-            if centro_cano <= centro_passaro < centro_cano + 4:
-                pontuacao += 1
-                SONS['ponto'].play()
+        centro_jogador = posicao_passaro_x + IMAGENS['jogador'][0].get_width() / 2
 
-        # Atualiza índices e movimentos
-        if (iteracao_loop + 1) % 3 == 0:
-            indice_jogador = next(gerador_indice_jogador)
-        iteracao_loop = (iteracao_loop + 1) % 30
-        base_x = -((-base_x + 100) % deslocamento_base)
+        for c_sup in canos_superiores:
+            centro_cano = c_sup['x'] + IMAGENS['cano'][0].get_width() / 2
+            
+            # Se o centro do jogador passou o centro do cano e o cano ainda não deu ponto
+            if centro_cano <= centro_jogador and not c_sup['passou']:
+                pontuacao += 1
+                c_sup['passou'] = True # Marca para não pontuar de novo
+                SONS['ponto'].play()
 
         # Gravidade do pássaro
         if velocidade_passaro_y < velocidade_max_passaro_y and not bateu_asa:
@@ -85,12 +83,15 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
         posicao_passaro_y += min(velocidade_passaro_y, BASE_Y - posicao_passaro_y - altura_passaro)
 
         # Movimenta Canos
+        nivel_velocidade = pontuacao // 2
+        velocidade_atual = VELOCIDADE_BASE - nivel_velocidade 
+
         for c_sup, c_inf in zip(canos_superiores, canos_inferiores):
-            c_sup['x'] += -4
-            c_inf['x'] += -4
+            c_sup['x'] += velocidade_atual
+            c_inf['x'] += velocidade_atual
 
         # Adiciona novos canos e remove os antigos
-        if 0 < canos_superiores[0]['x'] < 5:
+        if canos_superiores[-1]['x'] < LARGURA_DA_TELA / 2:
             novo_cano = obter_cano_aleatorio(IMAGENS['cano'])
             canos_superiores.append(novo_cano[0])
             canos_inferiores.append(novo_cano[1])
@@ -98,6 +99,12 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
         if canos_superiores[0]['x'] < -IMAGENS['cano'][0].get_width():
             canos_superiores.pop(0)
             canos_inferiores.pop(0)
+            
+        # Atualiza índices e movimentos
+        if (iteracao_loop + 1) % 3 == 0:
+            indice_jogador = next(gerador_indice_jogador)
+        iteracao_loop = (iteracao_loop + 1) % 30
+        base_x = -((-base_x - velocidade_atual) % deslocamento_base)
 
         # Desenha tudo
         TELA.blit(IMAGENS['fundo'], (0,0))
