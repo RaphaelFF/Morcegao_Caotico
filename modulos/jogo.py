fd82fa6e7bc081bc1e4d71727d1d90356dc5ac4d
RenatoDiasc <115375244+RenatoDiasc@users.noreply.github.com>
2026-02-28 19:08:00 -0300

    fix: correcao da variavel usada para lógica de transicao

--- modulos/jogo.py
+++ modulos/jogo.py
@@ -72,7 +72,7 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
                 SONS['ponto'].play()
 
         # --- LÓGICA DE TRANSIÇÃO SUAVE (Cano e Fundo) ---
-        alvo_cano = (pontuacao // TRASICAO) % len(LISTA_CANOS)
+        alvo_cano = (pontuacao // TRANSICAO) % len(LISTA_CANOS)
         if alvo_cano != indice_cano_atual and not transicionando_cano:
             indice_cano_novo = alvo_cano
             transicionando_cano = True
@@ -92,7 +92,7 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
         posicao_passaro_y += min(velocidade_passaro_y, BASE_Y - posicao_passaro_y - IMAGENS['jogador'][0].get_height())
 
         # Movimentação e Velocidade
-        velocidade_atual = VELOCIDADE_BASE - (pontuacao // TRASICAO)
+        velocidade_atual = VELOCIDADE_BASE - (pontuacao // TRANSICAO)
         for c_sup, c_inf in zip(canos_superiores, canos_inferiores):
             c_sup['x'] += velocidade_atual
             c_inf['x'] += velocidade_atual
@@ -115,7 +115,7 @@ def jogo_principal(TELA, RELOGIO_FPS, IMAGENS, SONS, MASCARAS_COLISAO, info_movi
         # --- RENDERIZAÇÃO COM FADE ---
         
         # 1. Fundo
-        indice_cenario = (pontuacao // TRASICAO) % len(IMAGENS['fundos']) 
+        indice_cenario = (pontuacao // TRANSICAO) % len(IMAGENS['fundos']) 
         TELA.blit(IMAGENS['fundos'][fundo_antigo_indice], (0, 0))
         if indice_cenario != fundo_antigo_indice:
             fundo_novo = IMAGENS['fundos'][indice_cenario].copy()
